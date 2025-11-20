3277334ff290dc4dc07ba3d331140db340d9bf2c
CYDNEY MILLER <cgmiller42@tntech.edu>
2025-11-20 11:51:47 -0600

    Create colors.py

---
+++ Stable Marriage Visualization/colors.py
@@ -0,0 +1,14 @@
+'''
+    Filename: Colors.py
+    Author: Cydney Miller
+    Date Created: 11/20/2025
+    Date Updated: 11/20/2025
+    Purpose: Class to store colors used for visualization.
+'''
+
+class bcolors:
+    MEN = '\033[38;5;200m'      # Pinkish
+    WOMEN = '\033[38;5;202m'    # Orange
+    PROPOSED = '\033[38;5;12m'  # Bright blue
+    ENGAGED = '\033[38;5;199m'  # Magenta
+    RESET = '\033[0m'
